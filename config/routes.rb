4c1b1e1775999bad513ba9b71514b5068d09faab
Jeremy Prevost <jprevost@mit.edu>
2017-07-26 16:50:57 -0400

    Adds sign in / sign out links

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,10 @@ Rails.application.routes.draw do
     :omniauth_callbacks => 'users/omniauth_callbacks'
   }
 
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new', as: :user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root to: 'static#index'
 end
