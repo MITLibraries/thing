dd72fc31a543a65dae922bad318c5454a2468ddd
Jeremy Prevost <jprevost@mit.edu>
2018-01-03 11:23:35 -0500

    Prevents username and email from being logged with exceptions

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -14,8 +14,8 @@ Rollbar.configure do |config|
   # `username`, and `email` methods to fetch those properties. To customize:
   # config.person_method = "my_current_user"
   # config.person_id_method = "my_id"
-  # config.person_username_method = "my_username"
-  # config.person_email_method = "my_email"
+  config.person_username_method = nil
+  config.person_email_method = nil
 
   # If you want to attach custom data to all exception and message reports,
   # provide a lambda like the following. It should return a hash.
