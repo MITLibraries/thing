fd5764ed0b900e4ca218da305821a34ca67ce941
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-01-12 16:08:51 -0500

    Add test of failed submission

--- test/controllers/thesis_controller_test.rb
+++ test/controllers/thesis_controller_test.rb
@@ -33,4 +33,29 @@ class ThesisControllerTest < ActionDispatch::IntegrationTest
     # Check assumption.
     assert_equal title, Thesis.last.title
   end
+
+  test 'rerender after failed submission' do
+    sign_in users(:yo)
+    orig_thesis_count = Thesis.count
+    title = 'Spacecraft avoidance: a short'
+    post '/thesis',
+         params: {
+           thesis: {
+             title: title,
+             abstract: 'Frook.',
+             # The missing department ids here should cause the form to fail.
+             degree_ids: [degrees(:one).id.to_s],
+             right_id: rights(:one).id.to_s,
+             graduation_year: (Time.current.year + 1).to_s,
+             graduation_month: 'December'
+           }
+         }
+    # We expect a 200 OK for the http call, even though the form submission is
+    # not a success.
+    assert_response :success
+    assert_equal('create', @controller.action_name)
+
+    # No new theses have been created.
+    assert_equal(orig_thesis_count, Thesis.count)
+  end
 end
