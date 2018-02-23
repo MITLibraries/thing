f2b90e3ee5313bd6a7ad00c7ded8011dfb32b5a9
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-02-23 16:58:42 -0500

    Add name field to Users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,6 +9,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  role       :string           default("basic")
+#  name       :string
 #
 
 require 'test_helper'
