738b3a9539a7edcbae39bc7631ae67a856641809
Andromeda Yelton <andromeda.yelton@gmail.com>
2018-02-26 15:18:40 -0500

    Unbreak developer auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,9 +13,13 @@ module Users
     # to make sure we're not on the production server.
     def developer
       raise 'Invalid Authentication' unless Rails.configuration.fake_auth_enabled
+
+      # User.from_omniauth will look in auth.info for a uid, but the fake auth
+      # actually has auth.uid, not auth.info.uid.
+      request.env['omniauth.auth'].info.uid = request.env['omniauth.auth'].uid
+
       @user = User.from_omniauth(request.env['omniauth.auth'])
       @user.admin = true
-      @user.uid = @user.email
       @user.save
       sign_in_and_redirect @user, event: :authentication
       flash[:notice] = "Welcome #{@user.email}!"
