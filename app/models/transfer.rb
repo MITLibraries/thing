5db57bd19e52d24681f3e601cadb01b56da8db27
Matt Bernhardt <mjbernha@mit.edu>
2020-12-02 15:29:00 -0500

    Adds better tests and annotated files
    123456789012345678901234567890123456789012345678901234567890123456789012
    
    ** Why are these changes being introduced:
    
    * Adding these tests brings the Transfer model more into line with the
      testing we have already for Degrees, Departments, etc. As the model
      gets more work, I expect that it will eventually have tests comparable
      to what the Thesis model has.
    * Having annotations in the files also brings us more in line with what
      has been done for other models.
    
    ** Document any side effects to this change:
    
    n/a

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: transfers
+#
+#  id         :integer          not null, primary key
+#  note       :text
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Transfer < ApplicationRecord
   belongs_to :user
 end
--- test/models/transfer_test.rb
+++ test/models/transfer_test.rb
@@ -1,8 +1,29 @@
+# == Schema Information
+#
+# Table name: transfers
+#
+#  id         :integer          not null, primary key
+#  note       :text
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class TransferTest < ActiveSupport::TestCase
-  # test 'valid transfer' do
-  #   transfer = transfers(:one)
-  #   assert(transfer.valid?)
-  # end
+  test 'valid transfer' do
+    transfer = transfers(:valid)
+    assert(transfer.valid?)
+  end
+
+  test 'needs valid user' do
+    transfer = transfers(:baduser)
+    assert(transfer.invalid?)
+  end
+
+  test 'note is not required' do
+    transfer = transfers(:valid)
+    transfer.note = nil
+    assert(transfer.valid?)
+  end
 end
