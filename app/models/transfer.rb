4b44146d738cc953bc2fb802a02f1493fd40e830
Matt Bernhardt <mjbernha@mit.edu>
2020-12-04 14:27:11 -0500

    Transfer graduation logic to transfer from thesis
    ** Why are these changes being introduced:
    
    * The Thesis model already has a lot of logic and tests around how to
      treat graduation information - years, months, validations, etc. Until
      we abstract this into a common concern, we need to replicate them in
      the Transfer model.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/ETS-41
    
    ** How does this address that need:
    
    * This copies, with minimal changes, the methods, validations, and tests
      for graduation dates from the Thesis model into the Transfer model.
    
    ** Document any side effects to this change:
    
    There is an increase in repeated code now. I expect that CodeClimate will
    complain, but fixing it will have to happen in a later commit.

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -12,4 +12,54 @@
 class Transfer < ApplicationRecord
   belongs_to :user
   belongs_to :department
+
+  attr_accessor :graduation_year, :graduation_month
+
+  VALIDATION_MSGS = {
+    graduation_year: 'Required - Please input your year of graduation.',
+    graduation_month: 'Required - Please select your month of graduation.',
+  }
+
+  validates :graduation_year, presence:
+    { message: VALIDATION_MSGS[:graduation_year] }
+  validate :valid_year?
+  validates :graduation_month, presence:
+    { message: VALIDATION_MSGS[:graduation_month] }
+  validate :valid_month?
+
+  VALID_MONTHS = ['February', 'May', 'June', 'September']
+
+  before_create :combine_graduation_date
+  after_find :split_graduation_date
+
+  scope :valid_months_only, lambda {
+    select { |t| VALID_MONTHS.include? t.grad_date.strftime('%B') }
+  }
+
+  # Ensures submitted graduation year is a four-digit integer, not less than
+  # the year of the Institute's founding.
+  # We expect that graduation_year will be a String (in which case to_s is a
+  # no-op), but if it's an Integer this will also work.
+  def valid_year?
+    return if (/^\d{4}$/.match(graduation_year.to_s) &&
+               graduation_year.to_i >= 1861)
+    errors.add(:graduation_year, 'Invalid graduation year')
+  end
+
+  def valid_month?
+    return if VALID_MONTHS.include?(graduation_month)
+    errors.add(:graduation_month,
+      'Invalid graduation month; must be May, June, September, or February')
+  end
+
+  # Combine the UI supplied month and year into a datetime object
+  def combine_graduation_date
+    self.grad_date = Time.zone.local(graduation_year.to_i,
+                                     Date::MONTHNAMES.index(graduation_month))
+  end
+
+  def split_graduation_date
+    self.graduation_year = grad_date.strftime('%Y')
+    self.graduation_month = grad_date.strftime('%B')
+  end
 end
--- test/models/transfer_test.rb
+++ test/models/transfer_test.rb
@@ -22,9 +22,95 @@ class TransferTest < ActiveSupport::TestCase
     assert(transfer.invalid?)
   end
 
+  test 'needs a department' do
+    transfer = transfers(:valid)
+    transfer.department = nil
+    assert(transfer.invalid?)
+  end
+
   test 'needs valid department' do
     transfer = transfers(:valid)
     transfer.department_id = 'boo'
     assert(transfer.invalid?)
+  end
+
+  test 'invalid without grad date' do
+    transfer = transfers(:valid)
+    transfer.grad_date = nil
+    transfer.graduation_month = nil
+    transfer.graduation_year = nil
+    assert(transfer.invalid?)
+  end
+
+  test 'grad year should be vaguely reasonable' do
+    transfer = transfers(:valid)
+
+    # Valid
+    transfer.graduation_year = '1861'
+    assert transfer.valid?
+
+    transfer.graduation_year = '2018'
+    assert transfer.valid?
+
+    transfer.graduation_year = 1861
+    assert transfer.valid?
+
+    transfer.graduation_year = 2018
+    assert transfer.valid?
+
+    # Invalid
+    transfer.graduation_year = '1860'
+    assert transfer.invalid?
+
+    transfer.graduation_year = '10'
+    assert transfer.invalid?
+
+    transfer.graduation_year = '10000'
+    assert transfer.invalid?
+
+    transfer.graduation_year = 'honeybadgers'
+    assert transfer.invalid?
+  end
+
+  test 'only May, June, September, and February are valid months' do
+    transfer = transfers(:valid)
+    transfer.grad_date = nil
+    transfer.graduation_year = 2020
+
+    transfer.graduation_month = 'January'
+    assert transfer.invalid?
+
+    transfer.graduation_month = 'February'
+    assert transfer.valid?
+
+    transfer.graduation_month = 'March'
+    assert transfer.invalid?
+
+    transfer.graduation_month = 'April'
+    assert transfer.invalid?
+
+    transfer.graduation_month = 'May'
+    assert transfer.valid?
+
+    transfer.graduation_month = 'June'
+    assert transfer.valid?
+
+    transfer.graduation_month = 'July'
+    assert transfer.invalid?
+
+    transfer.graduation_month = 'August'
+    assert transfer.invalid?
+
+    transfer.graduation_month = 'September'
+    assert transfer.valid?
+
+    transfer.graduation_month = 'October'
+    assert transfer.invalid?
+
+    transfer.graduation_month = 'November'
+    assert transfer.invalid?
+
+    transfer.graduation_month = 'December'
+    assert transfer.invalid?
   end
 end
