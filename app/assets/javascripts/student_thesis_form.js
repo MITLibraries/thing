5be15bf2a3097c72f21e0246845232e44bd8d470
Matt Bernhardt <mjbernha@mit.edu>
2021-04-09 16:16:51 -0400

    Set focus to previous supervisor field on removal
    ** Why are these changes being introduced:
    
    * When the user removes a thesis supervisor field, the focus is left
      undefined. This has been flagged during an accessibility review, with
      a preference stated that focus be set explicitly.
    * Separately, we have identified a bug in the current edit form where
      the "remove this supervisor" link is not being correctly hidden.
      These two issues share a need to accurately count how many fields are
      visible, because fields are not always removed - but hidden.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/ETD-272
    
    ** How does this address that need:
    
    * This defines a new function within student_thesis_form.js, to set
      focus on the first visible supervisor field. This function is called
      during the cocoon:after-remove hook.
    * The separate bug in the edit form shares a common concern with this
      focus work, which is to identify how many fields are visible. Our
      previous work here just counted "a.remove" links, which turns out to
      be inadequate because an edit form does not remove existing fields -
      just hides them.
    * Both routines now rely on a shared function that counts how many
      visible fields exist.
    
    ** Document any side effects to this change:
    
    Our use of javascript on the student-facing thesis forms is now more
    complex.

--- app/assets/javascripts/student_thesis_form.js
+++ app/assets/javascripts/student_thesis_form.js
@@ -15,9 +15,36 @@ function conditionalLicenseField() {
 // Hide/show "remove this supervisor" link if only one
 // field is present (one value is required)
 function hideOnlySupervisorLink() {
-  if ($("a.remove_fields").length == 1) {
+  var visible_fields = visibleAdvisorFields();
+  if ( visible_fields.length === 1 ) {
     $("a.remove_fields").addClass("only");
   } else {
     $("a.remove_fields").removeClass("only")
   }
+}
+
+// Sets the form's focus on the first visible supervisor
+// field. Called after one is removed, to ensure focus
+// is always defined.
+function focusOnFirstVisibleSupervisor() {
+  var visible_fields = visibleAdvisorFields();
+  $( $(visible_fields)[0] ).find("input[type=text]").focus();
+}
+
+// Shared function to count how many supervisor fields
+// are visible. This is needed because, when editing a
+// thesis record, the "remove this supervisor" link does
+// not actually remove the field from the DOM - so you
+// need to filter the list of fields by which have been
+// made invisible.
+//
+// This is called by both the focus and hideOnly functions
+// above.
+function visibleAdvisorFields() {
+  return $('div.advisor.nested-fields').filter(function() {
+    if ( $(this).attr('style')==='display: none;') {
+      return false;
+    }
+    return true;
+  });
 }
