7957c16ae984d0cc10b66ce8596b02b7f1db93b8
Jeremy Prevost <jprevost@mit.edu>
2021-04-09 09:21:18 -0400

    Handle nil thesis titles in title_helper
    Why are these changes being introduced:
    
    * Some registrar loaded data does not have a title
    * We need to display a list of theses to users if they have more than
      one thesis
    * The helper method to display "Untitled thesis" only handled empty
      strings
    * This change adds handling nils to that method as well.
    
    Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/ETD-273
    
    How does this address that need:
    
    * Returns our expected text for nil thesis titles
    
    Document any side effects to this change:
    
    * Added regression tests in the helper for both empty, nil and populated
      titles

--- app/helpers/thesis_helper.rb
+++ app/helpers/thesis_helper.rb
@@ -87,7 +87,7 @@ module ThesisHelper
   # Handles UI situations where a thesis may not have a title but we need to
   # provide a link via its (undefined) title.
   def title_helper(thesis)
-    return "Untitled thesis" if thesis.title.strip.length < 1
+    return "Untitled thesis" unless thesis.title.present?
     thesis.title
   end
 end
--- test/helpers/thesis_helper_test.rb
+++ test/helpers/thesis_helper_test.rb
@@ -163,4 +163,19 @@ class ThesisHelperTest < ActionView::TestCase
 
     assert_equal 2019, latest_year
   end
+
+  test 'title helper can handle nils' do
+    @thesis = Thesis.new
+    assert_equal 'Untitled thesis', title_helper(@thesis)
+  end
+
+  test 'title helper can empty titles' do
+    @thesis = Thesis.new(title: '')
+    assert_equal 'Untitled thesis', title_helper(@thesis)
+  end
+
+  test 'title helper passes title if populated' do
+    @thesis = Thesis.new(title: 'yolo title!')
+    assert_equal 'yolo title!', title_helper(@thesis)
+  end
 end
