b7c268e9679663de4d0bfa80a3dfad552315cd9e
Jeremy Prevost <JPrevost@users.noreply.github.com>
2021-12-23 11:57:00 -0500

    Initial exploration on how we might generate archivematica csv

---
+++ app/models/archivematica_metadata.rb
@@ -0,0 +1,177 @@
+# All metadata about the thesis must be in a single CSV file with a column for each metadata field and a row for each
+# file associated with the thesis.
+# Thesis-level metadata  (title, abstract, author, etc.) should be in the row for the thesis PDF file.
+#
+# Repeated fields must have a column for each instance of the field in the metadata.
+#
+# Note: If a rights field includes the copyright character, it must be UTF-8 encoded in the CSV file for Archivematica
+# to parse it correctly.
+#
+# Other files included in the CSV should have blank values for any fields not directly related to the file -- fields
+# associated with the thesis such as title, abstract, author should be blank, but fields specific to the file such as
+# BitstreamChecksumValue should have the value present in the row for that file
+class ArchivematicaMetadata
+  require 'csv'
+
+  def initialize(thesis)
+    @thesis = thesis
+    @csv_hash = {
+      headers: []
+    }
+    @thesis.files.each_with_index do |_f, i|
+      @csv_hash["f#{i}".to_sym] = []
+    end
+    non_repeating
+    dc_terms_is_part_of
+    dc_contributor_author
+    dc_identifier_orcid
+    dc_contributor_advisor
+    # dc.description.degree
+    # thesis.degree.name
+    # mit.thesis.degree
+    # dc.publisher
+    # dc.contributor.department
+    # dc.type
+    # dc.date.submitted
+    # dc.rights
+    # dc.rights
+    # dc.rights.uri
+  end
+
+  def non_repeating
+    @csv_hash[:headers] << ['filename', 'Level_of_DPCommitment', 'dc.title', 'dc.date.issued',
+                            'dc.description.abstract', 'dc.identifier.uri', 'BitstreamDescription',
+                            'BitstreamChecksumValue', 'BitstreamChecksumAlgorithm']
+    @thesis.files.each_with_index do |f, i|
+      @csv_hash["f#{i}".to_sym] << "data/#{f.blob.filename}"
+      @csv_hash["f#{i}".to_sym] << level_of_commitment(f)
+      @csv_hash["f#{i}".to_sym] << dc_title(f)
+      @csv_hash["f#{i}".to_sym] << dc_date_issued(f)
+      @csv_hash["f#{i}".to_sym] << dc_description_abstract(f)
+      @csv_hash["f#{i}".to_sym] << dc_identifier_uri(f)
+      @csv_hash["f#{i}".to_sym] << bitstream_description(f)
+      @csv_hash["f#{i}".to_sym] << bitstream_checksum_value(f)
+      @csv_hash["f#{i}".to_sym] << 'MD5'
+    end
+  end
+
+  def level_of_commitment(file)
+    return '' unless file.purpose == 'thesis_pdf'
+
+    'Level 3'
+  end
+
+  def dc_title(file)
+    return '' unless file.purpose == 'thesis_pdf'
+
+    @thesis.title
+  end
+
+  def dc_date_issued(file)
+    return '' unless file.purpose == 'thesis_pdf'
+
+    file.blob.created_at
+  end
+
+  def dc_description_abstract(file)
+    return '' unless file.purpose == 'thesis_pdf'
+
+    @thesis.abstract
+  end
+
+  def dc_identifier_uri(file)
+    return '' unless file.purpose == 'thesis_pdf'
+
+    @thesis.dspace_handle.to_s
+  end
+
+  def bitstream_description(file)
+    # NOTE: these file purposes are repeated from sqs_message.rb and should be moved to a common location
+    file_purposes = { 'thesis_pdf' => 'Thesis PDF', 'thesis_source' => 'Thesis source', 'thesis_supplementary_file' =>
+                      'Supplementary file', 'proquest_form' => 'Proquest form', 'signature_page' => 'Signature page' }
+    file_purposes[file.purpose]
+  end
+
+  def bitstream_checksum_value(file)
+    base64_to_hex(file.checksum)
+  end
+
+  # Duplicated logic from dspace_publication_results_job.rb and should be considered form shared location
+  def base64_to_hex(base64_string)
+    Base64.decode64(base64_string).each_byte.map { |b| format('%02x', b.to_i) }.join
+  end
+
+  # repeatable, take care to increment all file rows but only include details in thesis_pdf
+  def dc_terms_is_part_of
+    @thesis.departments.each do |dept|
+      @csv_hash[:headers] << 'dc.terms.isPartOf'
+
+      @thesis.files.each_with_index do |file, i|
+        @csv_hash["f#{i}".to_sym] << if file.purpose == 'thesis_pdf'
+                                       "AIC#Course_#{dept.code_dw}_theses"
+                                     else
+                                       ''
+                                     end
+      end
+    end
+  end
+
+  # repeatable, take care to increment all file rows but only include details in thesis_pdf
+  def dc_contributor_author
+    @thesis.authors.each do |author|
+      @csv_hash[:headers] << 'dc.contributor.author'
+
+      @thesis.files.each_with_index do |file, i|
+        @csv_hash["f#{i}".to_sym] << if file.purpose == 'thesis_pdf'
+                                       author.user.preferred_name
+                                     else
+                                       ''
+                                     end
+      end
+    end
+  end
+
+  # repeatable, take care to increment all file rows but only include details in thesis_pdf
+  def dc_identifier_orcid
+    @thesis.authors.each do |author|
+      @csv_hash[:headers] << 'dc.identifier.orcid'
+
+      @thesis.files.each_with_index do |file, i|
+        @csv_hash["f#{i}".to_sym] << if file.purpose == 'thesis_pdf'
+                                       author.user.orcid.to_s
+                                     else
+                                       ''
+                                     end
+      end
+    end
+  end
+
+  def dc_contributor_advisor
+    @thesis.advisors.each do |advisor|
+      @csv_hash[:headers] << 'dc.contributor.advisor'
+
+      @thesis.files.each_with_index do |file, i|
+        @csv_hash["f#{i}".to_sym] << if file.purpose == 'thesis_pdf'
+                                       advisor.name
+                                     else
+                                       ''
+                                     end
+      end
+    end
+  end
+
+  def to_csv
+    CSV.generate do |csv|
+      @csv_hash.each do |row|
+        csv << row
+      end
+    end
+
+    # uncomment to debug by writing to a file
+    # CSV.open('tmp/file.csv', 'wb') do |csv|
+    #   @csv_hash.each do |row|
+    #     csv << row[1].flatten
+    #   end
+    # end
+  end
+end
