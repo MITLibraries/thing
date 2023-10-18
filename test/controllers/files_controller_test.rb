3f044f6c8eec2d9ff314de2169219e57a1e00ab4
Jeremy Prevost <JPrevost@users.noreply.github.com>
2023-10-18 11:29:09 -0400

    Add edge case handling to file rename feature
    Why are these changes being introduced:
    
    * Creating duplicate filenames or renaming the wrong file should be
      detected and prevented
    
    Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/ETD-643
    
    How does this address that need:
    
    * Adds checks to prevent creating a duplicate filename when renaming
      a thesis attachment
    * Ensures the attachment being renamed is correctly associated with the
      thesis in the URL path or form submission or prevents renaming
    
    Document any side effects to this change:
    
    * report related test numbers changed when number of theses changed
    * some rubocop autoformatting in thesis_controller_test

--- test/controllers/files_controller_test.rb
+++ test/controllers/files_controller_test.rb
@@ -154,4 +154,92 @@ class FilesControllerTest < ActionDispatch::IntegrationTest
     assert_equal('renamed_a_pdf_too.pdf', @attachment.blob.filename.to_s)
     assert_equal('KADsjJnGD1sVUgvqyZOaRg==', @attachment.checksum)
   end
+
+  test 'creating a duplicate filename flashes a warning and does not rename file' do
+    sign_in users(:thesis_admin)
+    @thesis = theses(:rename_attachment_tests)
+    assert_equal(2, @thesis.files.count)
+    @attachment_one = @thesis.files.first
+    @attachment_two = @thesis.files.last
+
+    # unique filenames
+    assert(@thesis.files.map { |f| f.filename.to_s }.uniq.count == 2)
+
+    # rename one file to same as other
+    post rename_file_path(@thesis, @attachment_one),
+         params: { attachment: { filename: @attachment_two.filename.to_s } }
+    follow_redirect!
+    assert_equal thesis_process_path(theses(:rename_attachment_tests)), path
+
+    assert_select '.alert-banner.error',
+                  text: 'The new name you chose is the same as an existing name of a file attached to this thesis.', count: 1
+
+    @attachment_one.reload
+    @attachment_two.reload
+
+    # unique filenames (still)
+    assert(@thesis.files.map { |f| f.filename.to_s }.uniq.count == 2)
+  end
+
+  test 'renaming a file on a thesis with multiple files only renames the intended file' do
+    sign_in users(:thesis_admin)
+    @thesis = theses(:rename_attachment_tests)
+    assert_equal(2, @thesis.files.count)
+    @attachment_one = @thesis.files.first
+    @attachment_two = @thesis.files.last
+
+    # unique filenames
+    assert(@thesis.files.map { |f| f.filename.to_s }.uniq.count == 2)
+
+    # rename one file to same as other
+    post rename_file_path(@thesis, @attachment_one),
+         params: { attachment: { filename: 'hallo.pdf' } }
+    follow_redirect!
+    assert_equal thesis_process_path(theses(:rename_attachment_tests)), path
+
+    assert_select '.alert-banner.success',
+                  text: 'rename_my_attachments file hallo.pdf been updated.', count: 1
+
+    @attachment_one.reload
+    @attachment_two.reload
+
+    # unique filenames (still)
+    assert(@thesis.files.map { |f| f.filename.to_s }.uniq.count == 2)
+  end
+
+  test 'accessing rename form with non-matching thesis and attachment redirects to thesis page with warning' do
+    sign_in users(:thesis_admin)
+    @thesis = theses(:publication_review)
+    @different_thesis = theses(:rename_attachment_tests)
+    @attachment = @different_thesis.files.first
+    get "/file/rename/#{@thesis.id}/#{@attachment.id}"
+    assert_response :redirect
+
+    follow_redirect!
+    assert_equal thesis_process_path(theses(:publication_review)), path
+    assert_select '.alert-banner.error',
+                  text: 'The file to be renamed was not associated with the thesis being edited.', count: 1
+  end
+
+  test 'submitting rename form with non-matching thesis and attachment redirects to thesis page with warning' do
+    sign_in users(:thesis_admin)
+    @thesis = theses(:publication_review)
+    @different_thesis = theses(:rename_attachment_tests)
+    @attachment = @different_thesis.files.first
+
+    assert_equal('b_pdf.pdf', @attachment.blob.filename.to_s)
+
+    post rename_file_path(@thesis, @attachment),
+         params: { attachment: { filename: 'renamed_a_pdf_too.pdf' } }
+    follow_redirect!
+    assert_equal thesis_process_path(theses(:publication_review)), path
+
+    assert_select '.alert-banner.error',
+                  text: 'The file to be renamed was not associated with the thesis being edited.', count: 1
+
+    @attachment.reload
+
+    refute_equal('renamed_a_pdf_too.pdf', @attachment.blob.filename.to_s)
+    assert_equal('b_pdf.pdf', @attachment.blob.filename.to_s)
+  end
 end
