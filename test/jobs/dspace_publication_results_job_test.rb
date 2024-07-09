b1c46b583e25ac2dc0a586358da658fac52679e6
jazairi <16103405+jazairi@users.noreply.github.com>
2024-07-09 17:00:06 -0400

    Unskip publication results job tests and make message IDs unique
    All failing tests in the DspacePublicationResultsJob test are
    related to the stubbed message that involves a bad checksum. On
    further investigation, this message has the same `message_id` as
    another message.
    
    [The SQS API docs](https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_Message.html)
    specify that the `MessageId` property must be unique. It's possible
    that this was always the case, but it was only enforced in more
    recent versions of `aws-sdk-sqs`, which would explain why these
    tests only started erroring when we tried to upgrade the gem.

--- test/jobs/dspace_publication_results_job_test.rb
+++ test/jobs/dspace_publication_results_job_test.rb
@@ -26,7 +26,7 @@ class DspacePublicationResultsJobTest < ActiveJob::TestCase
                                       'SubmissionSource' => { string_value: 'ETD', data_type: 'String' } } },
 
               # success but thesis no longer has files locally
-              { message_id: 'id1a', receipt_handle: 'handle1a',
+              { message_id: 'id1b', receipt_handle: 'handle1a',
                 body: '{"ResultType": "success", "ItemHandle": "http://example.com/handle/123123123", "lastModified": "Thu Sep 09 17: 56: 39 UTC 2021", "Bitstreams": [{ "BitstreamName": "a_pdf.pdf", "BitstreamUUID": "fakeuuidshhhhh",  "BitstreamChecksum": { "value": "2800ec8c99c60f5b15520beac9939a46", "checkSumAlgorithm": "MD5"}}]}',
                 message_attributes: { 'PackageID' => { string_value: "etd_#{@valid_with_no_local_files.id}", data_type: 'String' },
                                       'SubmissionSource' => { string_value: 'ETD', data_type: 'String' } } },
@@ -94,7 +94,7 @@ class DspacePublicationResultsJobTest < ActiveJob::TestCase
     assert_equal 'Publication error', @bad_thesis.publication_status
     assert_equal 'Publication error', @no_handle_thesis.publication_status
     assert_equal 'Publication error', @invalid_status_thesis.publication_status
-    # assert_equal 'Publication error', @bad_checksum.publication_status # SQS mocks need to be updated
+    assert_equal 'Publication error', @bad_checksum.publication_status
   end
 
   test 'thesis handle is updated' do
@@ -106,11 +106,10 @@ class DspacePublicationResultsJobTest < ActiveJob::TestCase
     @bad_checksum.reload
 
     assert_equal 'http://example.com/handle/123123123', @good_thesis.dspace_handle
-    # assert_equal 'http://example.com/handle/123123123', @bad_checksum.dspace_handle # SQS mocks need to be updated
+    assert_equal 'http://example.com/handle/123123123', @bad_checksum.dspace_handle
   end
 
   test 'results hash is populated' do
-    skip('SQS mocks need to be updated')
     results = DspacePublicationResultsJob.perform_now
 
     # 6 total results confirms that the non-ETD message was skipped
@@ -168,13 +167,12 @@ class DspacePublicationResultsJobTest < ActiveJob::TestCase
     assert_includes results[:errors], "Couldn't find Thesis with 'id'=9999999999999"
 
     # bad checksum
-    # SQS mocks need to be updated
-    # assert_includes results[:errors], 'Thesis 532738922 with handle http://example.com/handle/123123123 was published ' \
-    #                                   'with non matching checksums. ETD checksums ' \
-    #                                   '["2800ec8c99c60f5b15520beac9939a46"] dspace checksums ["borkedchecksum"]. This ' \
-    #                                   'requires immediate attention to either manually replace the problem file in ' \
-    #                                   'DSpace or delete the entire thesis from DSpace to ensure that nobody is able ' \
-    #                                   'to download the broken file.'
+    assert_includes results[:errors], 'Thesis 532738922 with handle http://example.com/handle/123123123 was published ' \
+                                      'with non matching checksums. ETD checksums ' \
+                                      '["2800ec8c99c60f5b15520beac9939a46"] dspace checksums ["borkedchecksum"]. This ' \
+                                      'requires immediate attention to either manually replace the problem file in ' \
+                                      'DSpace or delete the entire thesis from DSpace to ensure that nobody is able ' \
+                                      'to download the broken file.'
 
     # no local files to checksum
     assert_includes results[:errors], 'Thesis 980190962 updated to status Publication error due to inability to ' \
@@ -205,7 +203,6 @@ class DspacePublicationResultsJobTest < ActiveJob::TestCase
   # The requirements are less strict than for preservation because any published thesis should be exported as MARC, even
   # if it's not valid by preservation standards.
   test 'only published theses are exported as MARC' do
-    skip('SQS mocks need to be updated')
     results = DspacePublicationResultsJob.perform_now
     assert_equal 3, results[:marc_exports].count
   end
